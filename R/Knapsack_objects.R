09248ebeaa8c2826c2b1afe6a82c3a0b92752b37
MartinSmel <43166833+MartinSmel@users.noreply.github.com>
2018-10-09 15:36:11 +0200

    Vignettes + Knapsack_object

---
+++ R/Knapsack_objects.R
@@ -0,0 +1,20 @@
+#' Knapsack_objects
+#'
+#' Creating a data set
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#'
+#' @export
+"knapsack_objects"
+
+set.seed(42)
+n <- 2000
+knapsack_objects <-
+  data.frame(
+    w=sample(1:4000, size = n, replace = TRUE),
+    v=runif(n = n, 0, 10000)
+  )
+save(knapsack_objects, file="data/knapsack_objects.RData")
