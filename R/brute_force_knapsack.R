9f9278981ae8961f8890fbe6def5ff55b88717f8
MartinSmel <43166833+MartinSmel@users.noreply.github.com>
2018-10-15 15:57:23 +0200

    brute_force parralelized

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -4,10 +4,12 @@
 #'
 #'@param x data.frame
 #'@param W integer
+#'@param parallel logical
 #'@return list of ideal value and elements that solves the problem
+#'@import parallel
 #'@export
 
-brute_force_knapsack <- function(x, W)
+brute_force_knapsack <- function(x, W, parallel=FALSE)
 {
   stopifnot(is.data.frame(x) == T)
   stopifnot(ncol(x)==2)
@@ -20,10 +22,19 @@ brute_force_knapsack <- function(x, W)
   x$label <- 1:n
   x <- x[which(x$w < W), ]
   n = nrow(x)
- 
+  if (parallel == TRUE) {
+    cluster = makeCluster(no_cores)
+    permutation <- parSapply(cluster, 1:(2^n-1), function(i) {intToBits(i)[1:n]}, simplify="array")
+    weight <- parSapply(cluster, 1:(2^n-1), function(i) {sum(as.numeric(permutation[,i])*x$w) }, simplify="array")
+    value <- parSapply(cluster, 1:(2^n-1), function(i) {sum(as.numeric(permutation[,i])*x$v) }, simplify="array")
+    stopCluster(cluster)
+  }
+  else
+  {
   permutation <- sapply(1:(2^n-1), function(i) { intToBits(i)[1:n]}, simplify="array")
   weight <- sapply(1:(2^n-1), function(i) { sum(as.numeric(permutation[,i])*x$w) }, simplify="array")
   value <- sapply(1:(2^n-1), function(i) { sum(as.numeric(permutation[,i])*x$v) }, simplify="array")
+  }
   for (i in 1:(2^n-1))  
   {
     if (ideal_value<value[[i]] && weight[[i]] < W)
